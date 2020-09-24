10ffed5c3f2d2f0dbb6ccf8c5e746e8d513def0b
advantechralph <advantechralph@gmail.com>
2020-09-24 13:10:18 +0800

    update version and fix the MCU 2.3 reboot long run testing failed problem.

--- macro.mk
+++ macro.mk
@@ -14,7 +14,7 @@ scripts=$(CURRDIR)/scripts/basic
 distro=ubuntu18044
 #bspname=$(shell echo "$(modelname)_$(distro)_$(shell date "+%Y%m%d")" | sed -e 's/^./\U&/g' -e 's/_./\U&/g')
 #bspname=$(modelname)_$(distro)_$(shell date "+%Y%m%d")
-bspname=WISE-710-A1_1G_r1_Ubuntu1804_20200922
+bspname=WISE-710-A1_1G_r1_Ubuntu1804_20200924
 packedrootfs=$(distro).tar.gz
 define releaseinfo
 $(shell date +"%Y%m%d%H%M%S")
