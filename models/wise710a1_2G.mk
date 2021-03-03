6a73ad838ba8cb5ba4efd9ebebbfa78e4ee15b98
Ralph Wang <ralph.wang@advantech.com.tw>
2021-03-03 16:49:19 +0800

    update for wise710a1 DDR3 2GB

--- models/wise710a1_2G.mk
+++ models/wise710a1_2G.mk
@@ -1,7 +1,7 @@
-ifeq ($(modelname),wise710a1_2g)
+ifeq ($(modelname),wise710a1_2G)
 bspname=WISE710A1-2GB-r1-Ubuntu1604-$(shell date +"%Y%m%d")
 rootfs=$(CURRDIR)/rootfs/wise710a1
-spl=$(CURRDIR)/spl/wise710a1_2g/SPL
-dtb:=$(CURRDIR)/dtb/imx6dl-wise710-a1.dtb
-u-boot:=$(CURRDIR)/u-boot/wise710a1_2g
+spl=$(CURRDIR)/spl/wise710a1_2G/SPL
+dtb=$(CURRDIR)/dtb/wise710a1/imx6dl-wise710-a1.dtb
+u-boot=$(CURRDIR)/u-boot/wise710a1_2G
 endif
